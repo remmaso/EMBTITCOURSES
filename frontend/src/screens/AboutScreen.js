21cb33dca26a666b137b046c96ce671e103382ae
remmaso <remmaso@yahoo.com>
2021-06-24 08:59:19 +0100

    updating of file and deletion

--- frontend/src/screens/AboutScreen.js
+++ frontend/src/screens/AboutScreen.js
@@ -73,48 +73,14 @@ const About = () => {
       </div>
     </div>
   </section>
-  <section id="icon-boxes" class="p-5">
-    <div className="container">
-      <div className="row mb-4">
-        <div className="col-md-4">
-          <div className="card bg-danger text-white text-center">
-            <div className="card-body">
-              <i className="fas fa-building fa-3x"></i>
-              <h1>Vision</h1>
-              To serve the mini, micro and small-scale enterprises (MMSE) and persons whose needs may not adequately be met by/through conventional Institutions/practices 
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="card bg-dark text-white text-center">
-            <div className="card-body">
-              <i className="fas fa-bullhorn fa-3x"></i>
-              <h1>Mission</h1>
-              To provide cutting-edge, tailor-made but unique solutions to our varied clientele in a timely manner; always aware that one-size-fits-all don’t work in this segment of the market
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="card bg-danger text-white text-center">
-            <div className="card-body">
-              <i className="fas fa-comments fa-3x"></i>
-              <h1>Core Values</h1>
-              Excellence Modeled on Beauty, Transparency, Ethical values and good Conscience
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    </section>
-
   <section id="home-heading" class="p-5">
     <div className="dark-overlay">
       <div className="row">
         <div className="col">
           <div className="container pt-5">
-            <h1>EMBTEC Gallery</h1>
-            <p className="d-none d-md-block"><h1>Vision</h1>
-              To serve the mini, micro and small-scale enterprises (MMSE) and persons whose needs may not adequately be met by/through conventional Institutions/practices </p>
+            <h1>EMPOWER YOURSELF</h1>
+            <p className="d-none d-md-block"><h1>WITH</h1>
+              Information Technology Skills  and Certification that is globally recognized in the industry by registering with us. Today!</p>
           </div>
         </div>
       </div>
